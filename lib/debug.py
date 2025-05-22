2c2d5f8264db9bae8a65c02dbd54ca1494278d7f
Larry <larryotieno51@gmail.com>
2025-05-22 20:28:52 +0300

    defining sqlalchemy

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
 
 from sqlalchemy_sandbox import Student
 
+Base = declarative_base()
+
+
 engine = create_engine('sqlite:///students.db')
+Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
